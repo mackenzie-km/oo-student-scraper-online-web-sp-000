71857383121e16c1a1f777ff86a9e9b779218ebe
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-22 18:53:48 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open("#{index_url}")
+    data = Nokogiri::HTML(html).css('div.roster-cards-container')
+    data_table = data.css('a').collect {|l| l.text}
+    data_table[0].split("\n").strip
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
